897fc88bc30351d1cc1bb8e906e10310a09e753d
JhonHYL <jhonrodrigohyl@gmail.com>
2025-04-26 00:18:33 +0200

    Metodos para generar contraseñas en la clase PasswordGenerator

--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -1,15 +1,36 @@
-import java.util.Scanner;
+import java.security.SecureRandom;
 
 public class PasswordGenerator {
-    //Lo hace Jhon
-    /*
-    Aquí puedes añadir un metodo para generar contraseñas fuertes automáticamente
-    Debera crear una contraseña en base a unos criterios dados por el usuario: Longitud, incluye x nª de mayus, incluye x nº de numeros, incluye x nº
-    de caracteres especiales, la longitud que halla solicitado el usuario...
 
-    crear objeto de
-    */
+    // Metodo para generar una clave maestra de 16 caracteres
+    public static String generarClaveMaestra() {
+        return generateRandomString(16);
+    }
 
+    // Mtodo para generar una contraseña normal con una longitud configurable
+    public static String generarPass(int length) {
+        return generateRandomString(length);  
+    }
 
+    // Metodo auxiliar para generar una cadena aleatoria
+    private static String generateRandomString(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            sb.append(characters.charAt(index));
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String claveMaestra = generarClaveMaestra();
+        String pass = generarPass(12);
+
+        System.out.println("Clave maestra: " + claveMaestra);
+        System.out.println("Contraseña normal: " + pass);
+    }
 }
-
