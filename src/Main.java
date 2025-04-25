cc7bb4f788c8908386116ee097b544d2c95ecff5
JhonHYL <jhonrodrigohyl@gmail.com>
2025-04-25 22:02:57 +0200

    Movido el metodo de pedirClaveMaestra al main para poderlo private, añadido menu de opciones en el main

--- src/Main.java
+++ src/Main.java
@@ -26,12 +26,76 @@
             }
 
             Scanner sc = new Scanner(System.in);
-            String clave = PasswordGenerator.pedirClaveMaestra(sc);
+            String clave = pedirClaveMaestra(sc);
 
             // 3. Instanciar el gestor con la clave
             PasswordManager gestor = new PasswordManager(passwordFile, clave);
 
+            //4. Salir
+            boolean salir = false;
+            while (!salir) {
+                mostrarMenu();
+                try {
+                    int opt = Integer.parseInt(sc.nextLine());
+                    switch (opt) {
+                        case 1:
+                            //Aqui llamar al metodo de crear contra cuando se cree
+                            break;
+                        case 2:
+                            //Aqui llamar al metodo de mostrar contra
+                            break;
+                        case 3:
+                            salir = true;
+                            break;
+                        default:
+                            System.out.println("Introduce una opción válida (1-3)");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Por favor, introduce un número.");
+                }
+            }
 
+            sc.close();
+        } 
+
+        private static String pedirClaveMaestra(Scanner sc) {
+            String clave = "";
+            boolean confirmada = false;
+
+            while (!confirmada) {
+                System.out.print("Introduce una clave de 16 caracteres para encriptar tus contrase\u00D1as: ");
+                clave = sc.nextLine();
+
+                while (clave.length() != 16) {
+                    System.out.println("La clave debe tener exactamente 16 caracteres.");
+                    System.out.print("Vuelve a introducir la clave: ");
+                    clave = sc.nextLine();
+                }
+
+                System.out.println("Has introducido la clave: " + clave);
+                System.out.println("Recuerda: debes usar siempre la misma clave para desencriptar.");
+
+                String respuesta = "";
+                System.out.print("¿Est\u00E1s seguro de usar esta clave? (S/N): ");
+                respuesta = sc.nextLine().trim().toLowerCase();
+
+                while (!respuesta.equals("s") && !respuesta.equals("n")) {
+                    System.out.print("Respuesta no v\u00E1lida. Por favor, introduce S o N: ");
+                    respuesta = sc.nextLine().trim().toLowerCase();
+                }
+
+                if (respuesta.equals("s")) {
+                    confirmada = true;
+                }
+            }
+
+            return clave;
+        }
+        private static void mostrarMenu() {
+            System.out.println("\n--- MENU DE OPCIONES ---");
+            System.out.println("1. Añadir contraseña");
+            System.out.println("2. Mostrar contraseña");
+            System.out.println("3. Salir");
+            System.out.print("Opción: ");
         }
     }
-
--- src/PasswordGenerator.java
+++ src/PasswordGenerator.java
@@ -7,40 +7,6 @@ public class PasswordGenerator {
     Debera crear una contraseña en base a unos criterios dados por el usuario: Longitud, incluye x nª de mayus, incluye x nº de numeros, incluye x nº
     de caracteres especiales, la longitud que halla solicitado el usuario...
     */
-    public static String pedirClaveMaestra(Scanner sc) {
-        String clave = "";
-        boolean confirmada = false;
-    
-        while (!confirmada) {
-            System.out.print("Introduce una clave de 16 caracteres para encriptar tus contrase\u00D1as: ");
-            clave = sc.nextLine();
-    
-            while (clave.length() != 16) {
-                System.out.println("La clave debe tener exactamente 16 caracteres.");
-                System.out.print("Vuelve a introducir la clave: ");
-                clave = sc.nextLine();
-            }
-    
-            System.out.println("Has introducido la clave: " + clave);
-            System.out.println("Recuerda: debes usar siempre la misma clave para desencriptar.");
-    
-            String respuesta = "";
-            System.out.print("¿Est\u00E1s seguro de usar esta clave? (S/N): ");
-            respuesta = sc.nextLine().trim().toLowerCase();
-    
-            while (!respuesta.equals("s") && !respuesta.equals("n")) {
-                System.out.print("Respuesta no v\u00E1lida. Por favor, introduce S o N: ");
-                respuesta = sc.nextLine().trim().toLowerCase();
-            }
-    
-            if (respuesta.equals("s")) {
-                confirmada = true;
-            }
-        }
-    
-        return clave;
-    }
-    
 
 
 }
